8aa52307dea43356005b670d8ae86a28cdf815e5
AmmonQ <aq229@byu.edu>
2023-02-14 18:27:40 -0700

    Reorganized some files to make the browserify stuff work better

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,5 +1,5 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-let constantsJS = require('../../shared/constants.js');
+let constantsJS = require('../shared/constants.js');
 
 // const BLUE = 0x0000FF;
 // const RED = 0xFF0000;
@@ -333,7 +333,7 @@ function update() {
         };
     }
 }
-},{"../../shared/constants.js":2}],2:[function(require,module,exports){
+},{"../shared/constants.js":2}],2:[function(require,module,exports){
 module.exports.Constants = class {
 
     static BLUE = 0x0000FF;
@@ -347,4 +347,4 @@ module.exports.Constants = class {
     static WIDTH = this.ROW_COL_SIZE * this.NUM_COLS + this.BORDER_SIZE * 2;
     static HEIGHT = this.ROW_COL_SIZE * this.NUM_ROWS + this.BORDER_SIZE * 2;
 }
-},{}]},{},[2,1]);
+},{}]},{},[1,2]);
